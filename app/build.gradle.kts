7925cd5af43d8be86db2a3e1f19984002aa34aab
Otávio Schmitt <otavio.schmitt@skedway.com>
2024-10-09 11:05:46 -0300

    Add library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("io.github.sceneview:sceneview:2.2.1")
 }
